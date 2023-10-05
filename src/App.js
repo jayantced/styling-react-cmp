ee75a38b001952fe269a5775bd0a16b0ec42f943
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-10-06 00:06:49 +0530

    gdfgd

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
+      // console.log(updatedGoals);
       updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
